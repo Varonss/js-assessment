d8ddbf95115c8102f5334eb6ea0a0ac2ad890d73
Varonss <martin.varona.84@gmail.com>
2018-07-26 19:16:43 -0500

    Done up to remove

--- app/arrays.js
+++ app/arrays.js
@@ -2,15 +2,35 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
+    for (var i = 0, len = arr.length; i < len; i++) {
+        if (arr[i] === item) {
+          return i;
+        }
+      }
 
+      return -1;
   },
 
   sum: function(arr) {
+    var sum = 0;
 
+   for (var i = 0, len = arr.length; i < len; i++) {
+     sum += arr[i];
+   }
+
+   return sum;
   },
 
   remove: function(arr, item) {
+    var ret = [];
 
+   for (var i = 0, len = arr.length; i < len; i++) {
+     if (arr[i] !== item) {
+       ret.push(arr[i]);
+     }
+   }
+
+   return ret;
   },
 
   removeWithoutCopy: function(arr, item) {
